28ff133b82c666be362eddc14d6c8de7f372bc32
weizuxiao <727897965@qq.com>
2026-09-11 15:22:14 +0800

    chore(extensions): 移除 copy-vsix-to-home 脚本 (打包不再 cp 到 ~/.numas/extensions)

--- extensions/html/scripts/package.js
+++ extensions/html/scripts/package.js
@@ -9,7 +9,6 @@
 const fs = require('fs')
 const path = require('path')
 const AdmZip = require('../../../registry/node_modules/adm-zip')
-const { copyVsixToHome } = require('../../scripts/copy-vsix-to-home')
 
 const ROOT = path.resolve(__dirname, '..')
 const SRC_DIST = path.join(ROOT, 'dist')           // 源 dist
@@ -61,4 +60,3 @@ zip.writeZip(OUT)
 fs.rmSync(STAGE, { recursive: true, force: true })
 
 console.log('[html] packaged:', OUT, '(' + (fs.statSync(OUT).size / 1024).toFixed(1) + ' KB)')
-copyVsixToHome(OUT, 'html')
